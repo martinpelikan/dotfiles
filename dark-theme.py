3cfb2bcb1828dc6dbc0ae7906afc4d5583d9d127
Martin Pelikan <martinpelikan@users.noreply.github.com>
2015-07-05 18:46:28 -0700

    Removed template docs from pudb's theme

--- dark-theme.py
+++ dark-theme.py
@@ -1,33 +1,3 @@
-# coding: utf-8
-
-"""Solarized theme for pudb."""
-
-# Supported 16 color values:
-#   'h0' (color number 0) through 'h15' (color number 15)
-#    or
-#   'default' (use the terminal's default foreground),
-#   'black', 'dark red', 'light green', 'brown', 'dark blue',
-#   'dark magenta', 'dark cyan', 'light gray', 'dark gray',
-#   'light red', 'light green', 'yellow', 'light blue',
-#   'light magenta', 'light cyan', 'white'
-#
-# Supported 256 color values:
-#   'h0' (color number 0) through 'h255' (color number 255)
-#
-# 256 color chart: http://en.wikipedia.org/wiki/File:Xterm_color_chart.png
-#
-# "setting_name": (foreground_color, background_color),
-
-# See pudb/theme.py
-# (https://github.com/inducer/pudb/blob/master/pudb/theme.py) to see what keys
-# there are.
-
-# Note, be sure to test your theme in both curses and raw mode (see the bottom
-# of the preferences window). Curses mode will be used with screen or tmux.
-
-
-# Based on XCode's midnight theme
-# Looks best in a console with green text against black background
 palette.update({
     # UI
     "header": ("black", "light blue", "standout"),
