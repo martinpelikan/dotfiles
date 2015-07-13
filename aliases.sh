a3f8eaf8ac14c97db92ff526406d951b30d6cd97
Martin Pelikan <martinpelikan@users.noreply.github.com>
2015-07-13 00:25:58 -0700

    Add more aliases, and don't alias .sh

--- aliases.sh
+++ aliases.sh
@@ -3,9 +3,14 @@ then
     alias -g vim='nvim'
 fi
 
+alias -s jpg=chromium
+alias -s pdf=chromium
+alias -s png=chromium
+
+alias -s wmv=vlc
+
 alias -s cfg=vim
 alias -s md=vim
 alias -s py=vim
 alias -s rst=vim
-alias -s sh=vim
 alias -s txt=vim
