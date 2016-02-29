67dd728c3d15ca2a6e61a4941fc49ed5f7289e11
Martin Pelikan <martinpelikan@users.noreply.github.com>
2016-02-28 17:40:29 -0800

    Update package lists

--- zsh/.aliases.sh
+++ zsh/.aliases.sh
@@ -7,6 +7,9 @@ then
     fi
 fi
 
+alias pip2upgrade="pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip2 install -U"
+alias pip3upgrade="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip3 install -U"
+
 alias :e='nvim'
 alias :E='nvim .'
 alias :q='exit'
