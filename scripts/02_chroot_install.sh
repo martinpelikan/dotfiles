5cde2154f9b64ab8c93cc52d459a957c50cbbea0
Martin Pelikan <martinpelikan@users.noreply.github.com>
2016-03-04 22:25:44 -0800

    Minor changes

--- scripts/02_chroot_install.sh
+++ scripts/02_chroot_install.sh
@@ -4,14 +4,14 @@ PASSWORD="ChangeMe321"
 TIMEZONE="America/Vancouver"
 NEW_USER="mpelikan"
 
-# Locale settings
+# Locale
 sed -i -e 's/#en_US\.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 ln -s /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
 hwclock --systohc --utc
 
-# Boot loader
+# Bootloader
 pacman -S --noconfirm grub os-prober
 grub-install "$DEVICE"
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -24,28 +24,47 @@ sed -i -e "s/localhost$/localhost $HOSTNAME/" /etc/hosts
 IFACE_NAME=$(ls /sys/class/net | expand | head -n1)
 systemctl enable dhcpcd@"$IFACE_NAME".service
 
-# Add Infinality repos and sign
+# Add Infinality repos
 sudo cat >> /etc/pacman.conf << EOF
+
 [infinality-bundle]
 Server = http://bohoomil.com/repo/$arch
+
 [infinality-bundle-fonts]
 Server = http://bohoomil.com/repo/fonts
 EOF
+# Add Infinality signatures
 sudo pacman-key -r 962DDE58
 sudo pacman-key --lsign-key 962DDE58
 
+# Install git
+pacman -S --noconfirm git
+
+# Install yaourt
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+
 # Totally safe installation of packages here
-pacman -S git
 git clone https://github.com/martinpelikan/dotfiles.git
 pacman -S --needed --noconfirm $(< dotfiles/packages/pacman.native.txt)
 pacman -S --needed --noconfirm $(< dotfiles/packages/pacman.external.txt)
 yaourt -S --needed --noconfirm $(< dotfiles/packages/pacman.foreign.txt)
-systemctl enable lightdm.service
+pip2 install -r dotfiles/packages/pip2.txt
+pip3 install -r dotfiles/packages/pip3.txt
 
-# Add my user
+# Create my user and add it to wheel group
 useradd -m -G wheel -s /bin/zsh "$NEW_USER"
-# Password
+sed -i -e 's/# %wheel ALL/%wheel ALL/' /etc/sudoers
+
+# XXX: Change these passwords!
 echo "root:$PASSWORD" | chpasswd
 echo "$NEW_USER:$PASSWORD" | chpasswd
 
-sed -i -e 's/# %wheel ALL/%wheel ALL/' /etc/sudoers
+# Enable services
+systemctl enable lightdm.service
