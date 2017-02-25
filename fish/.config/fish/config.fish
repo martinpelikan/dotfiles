ee0f76240ad4ba9ea998a398c8b35425e0283d5a
Martin Pelikan <martinpelikan@users.noreply.github.com>
2017-02-24 17:31:47 -0800

    Fixes fish dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,12 @@
+set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
-set -x XDG_DATA_HOME $HOME/.local/share
 
 set -x EDITOR nvim
 set -x VISUAL nvim
 
 set -x PATH $HOME/bin $PATH
+set -x GOPATH $HOME/go
 
-set -g theme_color_scheme solarized-dark
-set -g theme_nerd_fonts yes
+set theme_color_scheme solarized-dark
+set theme_nerd_fonts yes
