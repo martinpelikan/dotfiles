029e04901ba307f36b5f88edb2bed25717aa2115
Martin Pelikan <martinpelikan@users.noreply.github.com>
2017-07-08 17:05:36 -0700

    Re-add base/base-devel to package list.

--- fish/.config/fish/functions/paclist.fish
+++ fish/.config/fish/functions/paclist.fish
@@ -1,6 +1,9 @@
 function paclist --description 'List pacman explicitly installed packages not part of base/base-devel'
-    pacman -Qgq base base-devel | sort > /tmp/basepackages.txt
-    pacman -Qqe | sort > /tmp/installedpackages.txt
+    echo base > /tmp/installedpackages.txt
+    echo base-devel >> /tmp/installedpackages.txt
+    pacman -Qqe >> /tmp/installedpackages.txt
+    pacman -Qgq base base-devel | sort --output=/tmp/basepackages.txt
+    sort /tmp/installedpackages.txt --output=/tmp/installedpackages.txt
     comm -23 /tmp/installedpackages.txt /tmp/basepackages.txt
     rm -f /tmp/basepackages.txt /tmp/installedpackages.txt
 end
