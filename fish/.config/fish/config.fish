104e8f03acf878e128164c5a092bce5a56040e27
Martin Pelikan <martinpelikan@users.noreply.github.com>
2017-07-08 17:05:27 -0700

    Use neovim as MANPAGER

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,5 @@
 set -x FZF_LEGACY_KEYBINDINGS 0
 set -x fish_key_bindings fish_hybrid_key_bindings
-#bind \ed kill-word
 
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CONFIG_HOME $HOME/.config
@@ -8,12 +7,14 @@ set -x XDG_CACHE_HOME $HOME/.cache
 
 set -x PATH $HOME/bin $HOME/.gem/ruby/2.4.0/bin $HOME/bin/kaitai-struct-compiler-0.8-SNAPSHOT/bin $PATH
 set -x GOPATH $HOME/go
+set -x DOTFILES $HOME/dotfiles
 
+set -x MANPAGER "nvim -c 'set ft=man' -"
 set -x EDITOR nvim
 set -x VISUAL nvim
 
-# rg seems to be flaky in some circumstances
-# set -x FZF_DEFAULT_COMMAND 'rg --files --hidden'
-
 set theme_color_scheme solarized-dark
 set theme_nerd_fonts yes
+
+# rg seems to be flaky in some circumstances
+# set -x FZF_DEFAULT_COMMAND 'rg --files --hidden'
