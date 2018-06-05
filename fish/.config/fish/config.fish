1c90325b6f203224a596aa760447d3614db55054
Martin Pelikan <martinpelikan@users.noreply.github.com>
2018-06-05 00:04:48 -0700

    Remove Kaitai paths from fish env vars

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,7 @@ set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 
-set -x PATH $HOME/bin $HOME/.gem/ruby/2.4.0/bin $HOME/bin/kaitai-struct-compiler-0.8-SNAPSHOT/bin $PATH
+set -x PATH $HOME/bin $PATH
 set -x GOPATH $HOME/go
 set -x DOTFILES $HOME/dotfiles
 
