dd9fb9ca37db93277c2b37ba51ab9e98ecc3588d
Martin Pelikan <martinpelikan@users.noreply.github.com>
2019-10-02 00:38:15 -0700

    Start fish with clean slate

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,9 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 set -x FZF_LEGACY_KEYBINDINGS 0
 set -x fish_key_bindings fish_hybrid_key_bindings
 
@@ -13,13 +19,12 @@ set -x MANPAGER "nvim -c 'set ft=man' -"
 set -x EDITOR nvim
 set -x VISUAL nvim
 
-set -x PIP_REQUIRE_VIRTUALENV true
-
 set theme_color_scheme solarized-dark
 set theme_nerd_fonts yes
 
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
+# set -x PIP_REQUIRE_VIRTUALENV true
+# status --is-interactive; and source (pyenv init -|psub)
+# status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
 # rg seems to be flaky in some circumstances
 # set -x FZF_DEFAULT_COMMAND 'rg --files --hidden'
