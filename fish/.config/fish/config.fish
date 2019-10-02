6b9b400e9ce3b6c35fcb2cebbef30dedf0791dbe
Martin Pelikan <martinpelikan@users.noreply.github.com>
2019-10-02 01:07:24 -0700

    Remove fisher bootstrap and use aur instead
    Work out asdf-vm in favour of pyenv later

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,3 @@
-# if not functions -q fisher
-#     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-#     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-#     fish -c fisher
-# end
-
 set -x FZF_LEGACY_KEYBINDINGS 0
 set -x fish_key_bindings fish_hybrid_key_bindings
 
@@ -20,7 +14,7 @@ set -x EDITOR nvim
 set -x VISUAL nvim
 
 set theme_color_scheme solarized-dark
-set theme_nerd_fonts yes
+# set theme_nerd_fonts yes
 
 # set -x PIP_REQUIRE_VIRTUALENV true
 # status --is-interactive; and source (pyenv init -|psub)
