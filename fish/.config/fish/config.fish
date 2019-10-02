feed87d43bb3ab17a27c969a50e8e9090027576c
Martin Pelikan <martinpelikan@users.noreply.github.com>
2019-10-02 00:41:53 -0700

    Remove fisher bootstrap in favour of AUR

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
+# if not functions -q fisher
+#     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+#     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+#     fish -c fisher
+# end
 
 set -x FZF_LEGACY_KEYBINDINGS 0
 set -x fish_key_bindings fish_hybrid_key_bindings
