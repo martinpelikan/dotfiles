bde60f3d1cbf3744213ed9019c52bdc2e8cc2cd7
Martin Pelikan <martinpelikan@users.noreply.github.com>
2019-11-01 02:20:13 -0700

    Support more file types to preview in ranger

--- ranger/.config/ranger/scope.sh
+++ ranger/.config/ranger/scope.sh
@@ -90,9 +90,9 @@ handle_image() {
     local mimetype="${1}"
     case "${mimetype}" in
         # SVG
-        # image/svg+xml)
-        #     convert "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
-        #     exit 1;;
+        image/svg+xml)
+            convert "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
+            exit 1;;
 
         # Image
         image/*)
@@ -110,10 +110,10 @@ handle_image() {
             exit 7;;
 
         # Video
-        # video/*)
-        #     # Thumbnail
-        #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-        #     exit 1;;
+        video/*)
+            # Thumbnail
+            ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+            exit 1;;
         # PDF
         application/pdf)
             pdftoppm -f 1 -l 1 \
